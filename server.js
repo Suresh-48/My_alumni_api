ba5cd59ee3ec9c188ba667124c06609170653474
balachandran-18 <balachandran@aviartechservices.com>
2021-05-27 21:54:17 +0530

    [ALN] Port changed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ connect(database, {
 });
 
 // Start the server
-const port = process.env.PORT;
+const port = process.env.PORT || 5004;
 app.listen(port, () => {
   console.log(`Application is running on port ${port}`);
 });
