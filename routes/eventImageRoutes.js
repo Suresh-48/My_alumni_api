2e29bd45109febb1f4070dcf2c2babd7e4720d83
sanjayezhilmaran <sanjay.ezhilmaran@aviartechservices.com>
2021-06-15 17:54:43 +0530

    feature[ALM]-Image upload api for events

--- routes/eventImageRoutes.js
+++ routes/eventImageRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 const router = Router();
 
-import {updateEventImages, updateEventImage} from "../controllers/eventImageController.js";
+import {updateEventImage, postEventImage,PastEventImage} from "../controllers/eventImageController.js";
 
-router.route("/eventImage/").post(updateEventImages);
+router.route("/eventImage/").post(postEventImage);
 
 router.route("/eventImage/:id").put(updateEventImage);
+
+router.route("/pastimage/").get(PastEventImage);
+
 
 export default router;
