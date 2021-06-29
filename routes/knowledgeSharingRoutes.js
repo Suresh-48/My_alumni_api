17b11f29981fae05e7390552660fbe807085bac3
Gowtham <gowtham.nagarajan@aviartechservices.com>
2021-06-29 16:59:06 +0530

    post get fixed

--- routes/knowledgeSharingRoutes.js
+++ routes/knowledgeSharingRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
-import {createKnowledgeSharing, deleteKowledgeSharing, getAllKowledgeSharings, getKowledgeSharing, updateKowledgeSharing,} from '../controllers/knowledgeSharingController.js'
+import {createKnowledgeSharing, deleteKowledgeSharing, getAllKowledgeSharing, getKowledgeSharing, updateKowledgeSharing,} from '../controllers/knowledgeSharingController.js'
 
-router.route("/").get(getAllKowledgeSharings);
+router.route("/").get(getAllKowledgeSharing);
 router.route("/").post(createKnowledgeSharing);
 router.route("/:id").get(getKowledgeSharing);
 router.route("/:id").put(updateKowledgeSharing);
