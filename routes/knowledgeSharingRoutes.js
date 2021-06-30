911a38a017f254032c439b3bcdf6889c73fe8a5f
Vijay-Janakiraman <79345852+Vijay-Janakiraman@users.noreply.github.com>
2021-06-30 11:56:44 +0530

    knowledge Sharing Api

--- routes/knowledgeSharingRoutes.js
+++ routes/knowledgeSharingRoutes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 const router = Router();
 
-import {createKnowledgeSharing, deleteKowledgeSharing, getAllKowledgeSharings, getKowledgeSharing, updateKowledgeSharing,} from '../controllers/knowledgeSharingController.js'
+import {
+  createKnowledgeSharing,
+  deleteKowledgeSharing,
+  getAllKowledgeSharings,
+  getKowledgeSharing,
+  updateKowledgeSharing,
+} from "../controllers/knowledgeSharingController.js";
 
 router.route("/").get(getAllKowledgeSharings);
 router.route("/").post(createKnowledgeSharing);
 router.route("/:id").get(getKowledgeSharing);
 router.route("/:id").put(updateKowledgeSharing);
 router.route("/:id").delete(deleteKowledgeSharing);
-
-
 
 export default router;
