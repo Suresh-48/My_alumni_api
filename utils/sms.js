45a74ada2c013d42d6dac854db10c76c1e4d7a8c
balachandran-18 <balachandran@aviartechservices.com>
2021-07-11 18:53:41 +0530

    [ALM] SNS function changes

--- utils/sms.js
+++ utils/sms.js
@@ -1,29 +1,31 @@
 import AWS from "aws-sdk";
-import dotenv from "dotenv";
 
-dotenv.config({
-  accessKeyId: process.env.accessKeyId,
-  secretAccessKey: process.env.secretAccessKey,
-  region: process.env.region,
+// Config
+import { awsRegion, awsAccessKeyId, awsSecretAccessKey } from "../config.js";
+
+// Load AWS Config
+AWS.config.update({
+  accessKeyId: awsAccessKeyId,
+  secretAccessKey: awsSecretAccessKey,
+  region: awsRegion,
 });
-const sns = new AWS.SNS();
 
-export default function sendSms(message, to_number) {
-  sns.publish(
-    {
-      Message: `${message}`,
-      Subject: "Alumni",
-      PhoneNumber: `${to_number}`,
-    }
-    // (data,err)=>{
-    //   if(data){
-    //     console.log(data)
-    //   }else{
-    //     console.log(err)
-    //   }
-    // }
-  );
+const snsMessage = new AWS.SNS();
+
+/**
+ * Send SMS
+ *
+ * @param {*} message
+ * @param {*} to_number
+ */
+export default function sendSms(message, toNumber) {
+  snsMessage.publish({
+    Message: `${message}`,
+    Subject: "Alumni",
+    PhoneNumber: `${toNumber}`,
+  });
 }
+
 // export const bulkSms = (message, numbers) => {
 //   Promise.all(
 //     numbers.map((number) => {
